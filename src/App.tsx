67c320521a34c6b50609f0d63487a29d0243a472
Kevin James <kevinofficial95@gmail.com>
2026-03-09 12:06:57 +0000

    chore: add 24/7 support to hero highlights

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,7 @@ const featureGroups = [
 
 const launchHighlights = [
   "30-day free trial",
+  "24/7 support",
   "Built for churches of every size",
   "Fast setup for churches of every size",
   "Gift Aid-ready giving support",
